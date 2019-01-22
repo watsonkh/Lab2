57aeefe5cfabdb24cc9b5bd3169e57dabe8a3a87
Kyle Harrison Watson <watsonkh@vcu.edu>
2019-01-22 14:32:59 -0500

    Changed setup to create random colors

--- Setup.java
+++ Setup.java
@@ -1,6 +1,7 @@
-import bridges.connect.Bridges;
+import java.util.Random;
+
 import bridges.base.Array;
-import bridges.base.Element;
+import bridges.connect.Bridges;
 
 public class Setup {
 	public static void main(String[] args) throws Exception {
@@ -23,6 +24,12 @@ public class Setup {
 	    for (int i = 0; i < arraySize; i++) {
 	    	arr.getElement(i).setValue(i*i);
 	    	arr.getElement(i).setLabel(String.valueOf((i*i)));
+	    }
+	    
+	    Random rand = new Random();
+	    
+	    for (int i = 0; i < arraySize; i++) {
+	    	arr.getElement(i).getVisualizer().setColor(rand.nextInt(256),rand.nextInt(256),rand.nextInt(256),1);;
 	    }
 	    
 	    /* Tell BRIDGES which data structure to visualize */
